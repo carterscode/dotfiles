8bb6084da3f43fd15570499baa6cf48cbde95fc4
Carter Fields <carter@carterfields.com>
2022-03-15 19:23:41 -0700

    update install

--- brew-install-script.sh
+++ brew-install-script.sh
@@ -4,7 +4,7 @@
 # To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
 
 echo "Installing brew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Installing brew cask..."
 brew tap homebrew/cask
