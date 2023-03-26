d556c453e424f901b726c5d013f3cc9ad6f93158
Carter Fields <carter@carterfields.com>
2023-03-26 14:07:06 -0700

    Update brew-install-script.sh

--- brew-install-script.sh
+++ brew-install-script.sh
@@ -14,7 +14,9 @@ echo "Installing zsh and components ..."
 brew tap homebrew/cask-fonts 
 brew install --cask font-hack-nerd-font
 brew install --cask font-fira-code
+# exa— A modern replacement for ls
 brew install exa
+# starship.rs - cross-shell prompt
 brew install starship
 brew install zsh
 sudo dscl . -create /Users/$USER UserShell /opt/homebrew/bin/zsh
@@ -88,3 +90,35 @@ brew install speedtest-cli
 brew install tmux
 brew install --cask stats
 brew install --cask vlc
+# rg — ripgrep recursively searches directories for regex patterns
+brew install ripgrep
+# fd — a simple, fast and user-friendly alternative to find
+brew install fd
+# hyperfine - A command-line benchmarking tool
+brew install hyperfine
+# dust - a more intuitive version of du
+brew install dust
+# bat — A cat(1) clone with wings
+brew install bat
+# tldr — simplified and community-driven man pages
+brew install tldr
+# jump - faster fasd
+brew install jump
+# fzf — A command-line fuzzy finder
+brew install fzf
+$(brew --prefix)/opt/fzf/install
+brew cleanup
+
+
+
+
+
+
+
+
+
+
+
+
+
+
